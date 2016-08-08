dfb360705e84d6be1426852ea6e7a22d72a48115
Code <javasetraining@gmail.com>
2016-08-08 15:10:38 +0300

    Calculator class added

---
+++ src/com/calculator/Calculator.java
@@ -0,0 +1,46 @@
+package com.calculator;
+
+import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
+
+/**
+ * Created by MaRV on 8/8/2016.
+ */
+/*    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
+
+    import static org.slf4j.LoggerFactory.getLogger;*/
+
+
+    public class Calculator {
+        private static final int log = getLogger(Calculator.class);
+
+    private static int getLogger(Class<Calculator> calculatorClass) {
+        return 0;
+    }
+
+
+    public double getResult() {
+            throw new UnsupportedOperationException();
+        }
+
+        public void add(double first, double second) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void substract(double first, double second) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void multiple(double first, double second) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void div(double first, double second) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void expand(double first, double second) {
+            throw new UnsupportedOperationException();
+        }
+    }
