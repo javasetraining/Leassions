a2a0573b23ce0ad59969061638bbc737a34c555d
Code <javasetraining@gmail.com>
2016-08-25 15:46:53 +0300

    test for strings added

---
+++ src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * Created by MaRV on 8/24/2016.
+ */
+public class Test {
+
+
+        String name;
+        public static void main(String[] args) {
+            int [][] array = {{0},{0, 1}, {0, 2, 4}, {0, 3, 6, 9}, {0, 4, 8, 12, 16}};
+            System.out.print(array [4][1]);
+            System.out.print(array [1][1]);
+            System.out.print(array [1][4]);}}
