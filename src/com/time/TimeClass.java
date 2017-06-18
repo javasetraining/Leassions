6fb3791aa0800e867dc45a47375e3f9dbff89742
Dmitry Tkhorik <javasetrainitg@gmail.com>
2017-06-18 23:05:35 +0300

    begin to fill time and date methods

---
+++ src/com/time/TimeClass.java
@@ -0,0 +1,13 @@
+package com.time;
+
+
+public class TimeClass {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public void setTime(int h, int m, int s) {
+//37 tutorial newboston academy
+    }
+
+}
